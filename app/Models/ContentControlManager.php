03b0d5eebde4851289805aecaca4195b162a190b
Al-sany Samil <alsanysamil@gmail.com>
2020-12-22 23:45:36 +0600

    content control manager model added

---
+++ app/Models/ContentControlManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContentControlManager extends Model
+{
+    use HasFactory;
+    protected $table = 'contentcontrolmanager';
+    public $timestamps = false;
+}
