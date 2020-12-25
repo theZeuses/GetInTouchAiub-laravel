f240ab586e36eb0346b8ca05435a71de7c7c7509
Al-sany Samil <alsanysamil@gmail.com>
2020-12-25 20:49:45 +0600

    dispatcher

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\contentControllerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return redirect('/login');
+});
+
+Route::get('/login', 'loginController@index');
+Route::post('/login', 'loginController@verify');
+Route::get('/logout', 'logoutController@index');
+
+Route::group(['middleware' => ['sess']], function () {
+    Route::group(['middleware' => ['ContentController']], function () {
+        Route::get('/contentcontroller', 'contentControllerController@home')->name('contentController.home');
+
+        Route::get('/contentcontroller/post/request', 'contentControllerController@postRequest')->name('contentController.postRequest');
+        Route::get('/contentcontroller/post/approve/{id}', 'contentControllerController@approvePost')->name('contentController.approvePost');
+        Route::get('/contentcontroller/post/decline/{id}', 'contentControllerController@a]declinePost')->name('contentController.declinePost');
+        Route::get('/contentcontroller/analyzeposter/{pid}/{gid}', 'contentControllerController@analyzePoster')->name('contentController.analyzePoster');
+
+        Route::get('/contentcontroller/announcement', 'contentControllerController@announcement')->name('contentController.announcement');
+        Route::get('/contentcontroller/announcement/create', 'contentControllerController@createAnnouncement')->name('contentController.createAnnouncement');
+        Route::post('/contentcontroller/announcement/create', 'contentControllerController@storeAnnouncement');
+        Route::get('/contentcontroller/announcement/update/{id}', 'contentControllerController@updateAnnouncement')->name('contentController.updateAnnouncement');
+        Route::get('/contentcontroller/announcement/delete/{id}', 'contentControllerController@deleteAnnouncement')->name('contentController.deleteAnnouncement');
+
+        Route::get('/contentcontroller/users', 'contentControllerController@users')->name('contentController.userList');
+
+        Route::get('/contentcontroller/profile', 'contentControllerController@profile')->name('contentController.profile');
+
+        Route::get('/contentcontroller/reports', 'contentControllerController@reports')->name('contentController.reports');
+
+        Route::get('/contentcontroller/contribution', 'contentControllerController@contribution')->name('contentController.contribution');  
+    });
 });
