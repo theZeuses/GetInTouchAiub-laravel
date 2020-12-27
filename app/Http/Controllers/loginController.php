c75191a61889637e1f2fce7b858ce78acbce7456
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2020-12-28 01:44:47 +0600

    login and logout

---
+++ app/Http/Controllers/loginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class loginController extends Controller
+{
+    public function index(){
+    	return view('contentController.login');
+    }
+
+    public function verify(Request $req){
+
+        $user  = User::where('userid', $req->username)
+                        ->where('password', $req->password)
+                        ->first();
+        echo $req->username;
+        echo $req->password;
+        echo $user;
+    	if($user != null){
+
+            $req->session()->put('username', $req->username);
+            $req->session()->put('type', $user->usertype);
+            
+            if($req->session()->get('type') == "Content Control Manager"){
+                return redirect(route('contentController.home'));
+            }elseif($req->session()->get('type') == "General User"){
+                return redirect(route('generalUser.home'));
+            }
+            else{
+                return redirect('/login');
+            }
+    	}else{
+    		return redirect('/login');
+    	}
+    }
+}
