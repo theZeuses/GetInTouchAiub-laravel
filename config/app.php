2ee6d83459a896a385a7e1950ec24bc6e222a92d
Al-sany Samil <alsanysamil@gmail.com>
2020-12-30 02:39:44 +0600

    third party pakage added

--- config/app.php
+++ config/app.php
@@ -226,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Barryvdh\Snappy\Facades\SnappyPdf::class,
+        'SnappyImage' => Barryvdh\Snappy\Facades\SnappyImage::class,
 
     ],
 
