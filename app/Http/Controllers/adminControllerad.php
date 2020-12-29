477fb3883830fdb945f3657b3c1d9c84057db916
Shahriyar-Shazib <18-36610-1@student.aiub.edu>
2020-12-30 00:44:14 +0600

    Search CC ajax, json

--- app/Http/Controllers/adminControllerad.php
+++ app/Http/Controllers/adminControllerad.php
@@ -335,4 +335,22 @@ class adminControllerad extends Controller
         }
         return response()->json(['result'=>$result]);
     }
+    public function searchcc($key){
+        $string=json_decode($key);
+        error_log($string->key);
+        if(strlen($string->key)>0){
+            $result=ContentController::where('ccid','LIKE','%'.$string->key.'%' )
+                            ->orWhere('name','LIKE','%'.$string->key.'%')
+                            ->orWhere('email','LIKE','%'.$string->key.'%')
+                            ->orWhere('gender','LIKE','%'.$string->key.'%')
+                            ->orWhere('dob','LIKE','%'.$string->key.'%')
+                            ->orWhere('address','LIKE','%'.$string->key.'%')
+                            //->where('accountstatus','Active')
+                            ->get();
+        }
+        else{
+            $result=ContentController::where('accountstatus','Active')->get();
+        }
+        return response()->json(['result'=>$result]);
+    }
 }
