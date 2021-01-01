e7f6e79ad37fdc61ee3aea0430154bb99f14afb3
NayeemHasan807 <moonnayeem87@gmail.com>
2021-01-01 20:55:17 +0600

    Update profile validation

---
+++ app/Http/Requests/updateProfileRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class updateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3',
+            'email' => 'required|email:rfc,dns',
+            'dob' => 'required|date_format:Y-m-d',
+            'address' => 'required|min:5',
+            'profilepicture' => 'image|mimes:jpg,bmp,png'
+        ];
+    }
+
+    public function messages(){
+        return [
+            'name.required'=> "can't left empty...."
+        ];
+    }
+}
