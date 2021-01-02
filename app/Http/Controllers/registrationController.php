59bb2364c0caa4dcb14af6c4ffd447dc3d24c05c
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2021-01-02 22:09:38 +0600

    registrationController

---
+++ app/Http/Controllers/registrationController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\RegistrationRequest;
+use App\Http\Requests\registrationFormRequest;
+
+class registrationController extends Controller
+{
+    public function registrationform(){
+    	return view('generalUser.registrationForm');
+    }
+
+    public function signup(registrationFormRequest $req){
+        $file = $req->file('image');
+        $filename = $file->getClientOriginalName().time();
+        if($file->move('assets/generalUser/profilepicture', $filename)){
+			$rr = new RegistrationRequest();
+			$rr->guid = $req->guid;
+			$rr->name = $req->name;
+			$rr->email = $req->email;
+			$rr->gender = $req->gender;
+			$rr->dob = $req->dob;
+			$rr->address = $req->address;
+			$rr->profilepicture = "/assets/generalUser/profilepicture/".$filename;
+			$rr->userstatus = $req->userstatus;
+			if($rr->save())
+			{
+				return redirect('/login');
+			}
+			else
+			{
+				echo 'Failed to submit registration request!';
+			}
+
+		}else{
+			echo "Failed to submit";
+		}
+    }
+}
