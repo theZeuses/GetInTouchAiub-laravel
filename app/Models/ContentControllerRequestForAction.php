cd5506949f3ab60122bbef0a712216b6509f9793
NayeemHasan807 <moonnayeem87@gmail.com>
2021-01-03 02:16:13 +0600

    CCrequest for action and gutext model

---
+++ app/Models/ContentControllerRequestForAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class ContentControllerRequestForAction extends Model
+{
+
+    protected $table = 'ccrequestforaction';
+    protected $primaryKey = "id";
+	public $timestamps = false;
+
+}
