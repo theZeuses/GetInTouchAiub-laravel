e43014c521e604ae3475acf23ce8c7cb175cd112
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2021-01-03 14:53:26 +0600

    receivedtext

---
+++ app/Models/GuText.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class GuText extends Model
+{
+
+    protected $table = 'gutext';
+    protected $primaryKey = "id";
+	public $timestamps = false;
+
+}
