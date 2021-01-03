2d0137c5ad6fb1d958b06e1743b5ffc5b1b40393
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2021-01-03 15:25:41 +0600

    notifications

---
+++ app/Models/AdminNotice.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class AdminNotice extends Model
+{
+
+    protected $table = 'adminnotice';
+    protected $primaryKey = "id";
+	public $timestamps = false;
+
+}
