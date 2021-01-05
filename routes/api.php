477cabc64dd9f641c469f8142dfb6562ebd84544
Al-sany Samil <alsanysamil@gmail.com>
2021-01-05 10:42:57 +0600

    api routes dispatcher added

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/contentcontroller/action/accepted', 'contentControllerController@requestForActionAccepted');    
+Route::post('/contentcontroller/action/rejected', 'contentControllerController@requestForActionRejected');
